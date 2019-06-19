b6f8deb9e72fb3d464b307e86d77c348370978bd
Grzegorz <grzegorz.lulewicz@interia.pl>
2019-06-19 21:42:41 +0200

    Add spring boot

--- src/main/java/com/github/lulewiczg/controller/Main.java
+++ src/main/java/com/github/lulewiczg/controller/Main.java
@@ -2,15 +2,14 @@ package com.github.lulewiczg.controller;
 
 import java.nio.charset.Charset;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.config.AppenderRef;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.github.lulewiczg.controller.server.ControllerServer;
 import com.github.lulewiczg.controller.server.Settings;
@@ -22,7 +21,8 @@ import com.github.lulewiczg.controller.ui.ServerWindow;
  *
  * @author Grzegurz
  */
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
 
     private static final String CONSOLE = "console";
 
@@ -37,19 +37,13 @@ public class Main {
         Configuration config = ctx.getConfiguration();
         PatternLayout layout = PatternLayout.newBuilder().withPattern("[%d{dd.MM.YYYY HH:mm:ss}] [%p] - %m%ex%n")
                 .withCharset(Charset.defaultCharset()).build();
-        Appender consoleAppender = ConsoleAppender.createDefaultAppenderForLayout(layout);
-        consoleAppender.start();
-        AppenderRef ref = AppenderRef.createAppenderRef("CONSOLE_APPENDER", null, null);
-        AppenderRef[] refs = new AppenderRef[] { ref };
-        LoggerConfig loggerConfig = LoggerConfig.createLogger(false, Level.ALL, "LOGGER", "", refs, null, config, null);
-        loggerConfig.addAppender(consoleAppender, null, null);
+        LoggerConfig loggerConfig = config.getRootLogger();
         if (window) {
             JTextAreaAppender windowAppender = JTextAreaAppender.createAppender("SWING_APPENDER", 0, false, layout, null);
             windowAppender.start();
             loggerConfig.addAppender(windowAppender, null, null);
             config.addAppender(windowAppender);
         }
-        config.addAppender(consoleAppender);
         config.addLogger("", loggerConfig);
         ctx.updateLoggers(config);
     }
@@ -60,6 +54,11 @@ public class Main {
      * @param args
      */
     public static void main(String... args) {
+        new SpringApplicationBuilder(Main.class).headless(false).run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
         Settings settings = Settings.loadSettigs();
         if (args.length >= 1) {
             configureLogger(false);
