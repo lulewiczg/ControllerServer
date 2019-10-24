3d672c9b7b24820cee6bfaf22abf786d40500ce7
Grzegorz <grzegorz.lulewicz@interia.pl>
2019-10-24 15:57:38 +0200

    Experimental REST API

---
+++ src/main/java/com/github/lulewiczg/controller/controllers/MouseController.java
@@ -0,0 +1,32 @@
+package com.github.lulewiczg.controller.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.lulewiczg.controller.actions.processor.ControllingService;
+
+@RestController
+@RequestMapping("/rest/mouse")
+public class MouseController {
+
+    @Autowired
+    private ControllingService service;
+
+    @PostMapping("move")
+    public void move(long x, long y) {
+        service.getMouseService().move(x, y);
+    }
+
+    @PostMapping("press")
+    public void press(int key) {
+        service.getRobot().mousePress(key);
+    }
+
+    @PostMapping("release")
+    public void release(int key) {
+        service.getRobot().mouseRelease(key);
+    }
+
+}
